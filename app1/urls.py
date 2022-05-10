d0e75c5c4664223d230cd46b9b4ffdf11675af71
nsblackK <nsblack.2001@gmail.com>
2022-05-10 21:19:31 +0530

    both app working

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-path('login', views.login, name="login")
+    path('', views.buy, name="buy"),
+    path('login', views.login, name="login"),
+    path('sell', views.sell, name="sell"),
+    path('description', views.description, name="description"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
+def buy(request):
     return render(request, 'productPage.html');
 def login(request):
     return render(request, 'login_page.html');
+def sell(request):
+    return render(request, 'sales_page.html');
+def description(request):
+    return render(request, 'new_product_desc.html');
