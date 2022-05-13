ae55fe73a62eced799ea6c0be8bff3dd2df871da
nsblackK <nsblack.2001@gmail.com>
2022-05-13 16:17:00 +0530

    updated navbar

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database1',
+        'USER' : 'root',
+        'PASSWORD' : 'root@123',
+        'HOST':'localhost',
+        'PORT': '3306',
     }
 }
 
